46c2db89abca93e83bd0e0f611ede5f0d023db74
Micah Elizabeth Scott <micah@misc.name>
2017-06-08 00:32:49 +0200

    LED thing

--- led-tester.py
+++ led-tester.py
@@ -13,14 +13,26 @@ MAX_LEDS = 200
 
 
 def shader(frame):
-    def pixel(n):
-        blip = math.pow(math.sin(n / 30.0 + frame * 0.02) * 0.5 + 0.5, 40.0)
-        tint = (0.8, 1.0, 0.2)
 
-        bright = int(max(0, min(31, 0.5 + 31 * math.pow(blip, 8.0))))
-        r = int(max(0, min(255, 0.5 + 0xff * blip * tint[0] + 0.5)))
-        g = int(max(0, min(255, 0.5 + 0xff * blip * tint[1] + 0.5)))
-        b = int(max(0, min(255, 0.5 + 0xff * blip * tint[2] + 0.5)))
+    blips = [
+        (frame * 0.02,  (0.8, 1.0, 0.2)),
+        (frame * 0.03,  (0.2, 0.6, 0.9)),
+        (-frame * 0.01, (0.9, 0.3, 0.2)),
+    ]
+
+    def pixel(n):
+
+        br = [ math.pow(math.sin(n / 30.0 - center), 40.0) for center, color in blips ]
+
+        rgb = [
+            sum( br[i] * tint[ch] for i, (center, tint) in enumerate(blips) )
+            for ch in range(3)   
+        ]
+
+        bright = int(max(0, min(31, 1.0 + 10 * math.pow(sum(rgb), 4.5))))
+        r = int(max(0, min(255, 0.5 + 0xff * rgb[0])))
+        g = int(max(0, min(255, 0.5 + 0xff * rgb[1])))
+        b = int(max(0, min(255, 0.5 + 0xff * rgb[2])))
 
         return 0xE0000000 | (bright << 24) | (r << 16) | (g << 8) | b
     return pixel
@@ -28,7 +40,14 @@ def shader(frame):
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     frame = 0
+    last_t = 0
     while True:
+        if (frame & 0xf) == 0:
+            t = time.time()
+            dt = t - last_t
+            last_t = t
+            print("fps ~= ", 16.0 / dt)
+
         pixels = list(map(shader(frame), range(MAX_LEDS)))
         s.sendto(MSG_LEDS + struct.pack('>' + MAX_LEDS * 'I', *pixels), FLYER)
         time.sleep(1/100.0)
