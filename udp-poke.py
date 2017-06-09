77d0bec2558059619a8cbb9836b94477f26dc768
Micah Elizabeth Scott <micah@misc.name>
2017-06-09 07:17:11 +0200

    udp-poke should go anywhere

--- udp-poke.py
+++ udp-poke.py
@@ -6,13 +6,14 @@ import time
 import binascii
 
 PORT = 9024
-CONTROLLER = ('10.32.0.1', PORT)
-FLYER = ('10.32.0.8', PORT)
+
+delay = float(sys.argv[1])
+dest = (sys.argv[2], PORT)
+packet = binascii.a2b_hex(sys.argv[3])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(CONTROLLER)
 
 while True:
-    s.sendto(binascii.a2b_hex(sys.argv[2]), FLYER)
-    time.sleep(float(sys.argv[1]))
+    s.sendto(packet, dest)
+    time.sleep(delay)
 
