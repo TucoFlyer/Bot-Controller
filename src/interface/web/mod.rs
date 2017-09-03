c7153b519118d86f753322c6b83d6ec430c22e31
Micah Elizabeth Scott <micah@misc.name>
2017-09-02 20:40:46 -0700

    Enable quiet zone
    If presenting this in a web UI this may be redundant but in a terminal
    it allows the code to scan even if your desktop background is contrasty.

--- src/interface/web/mod.rs
+++ src/interface/web/mod.rs
@@ -22,11 +22,11 @@ pub fn start(bus: &Bus) {
 
 fn make_qr_code(url: &str) -> String {
     let code = QrCode::new(url).unwrap();
-    code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build()
+    code.render::<char>().quiet_zone(true).module_dimensions(2, 1).build()
 }
 
 fn make_connect_string(url: &str) -> String {
-    format!("{}\n\n{}\n", url, make_qr_code(url))
+    format!("{}\n{}\n", url, make_qr_code(url))
 }
 
 fn store_connect_string(s: &str, path: &str) -> io::Result<()> {
