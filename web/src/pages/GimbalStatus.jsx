1960264408988d6d9b01c4e93e595e8ef65d0d07
Micah Elizabeth Scott <micah@misc.name>
2017-11-15 16:41:49 -0800

    Gimbal page tidy

--- web/src/pages/GimbalStatus.jsx
+++ web/src/pages/GimbalStatus.jsx
@@ -19,7 +19,21 @@ export default (props) => {
             Stabilization motors OFF
         </GimbalMotorButton>
 
-        <h4>Gimbal controller status</h4>
+        <h4>Control Outputs</h4>
+
+        <h6>Yaw/Pitch rate control</h6>
+        <Chart>
+            <Series
+                strokeStyle='#a22'
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.rates[0] }
+                trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
+            <Series
+                strokeStyle='#22a'
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.rates[1] }
+                trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
+        </Chart>
+
+        <h4>Sensors</h4>
 
         <h6>Yaw angle</h6>
         <Chart>
@@ -35,17 +49,35 @@ export default (props) => {
                 trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
         </Chart>
 
-        <h6>Yaw/Pitch rate control</h6>
+        <h4>Hold status</h4>
+
+        <h6>Hold active flags</h6>
+        <Chart height="20" minValue="0" maxValue="1">
+            <Series
+                strokeStyle='#a22'
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.hold_active[0] | 0}
+                trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
+        </Chart>
+        <Chart height="20" minValue="0" maxValue="1">
+            <Series
+                strokeStyle='#22a'
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.hold_active[1] | 0}
+                trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
+        </Chart>
+
+        <h6>Current hold angles</h6>
         <Chart>
             <Series
                 strokeStyle='#a22'
-                value={ (model) => model.gimbal_status.message.GimbalControlStatus.rates[0] }
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.hold_angles[0] }
                 trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
             <Series
                 strokeStyle='#22a'
-                value={ (model) => model.gimbal_status.message.GimbalControlStatus.rates[1] }
+                value={ (model) => model.gimbal_status.message.GimbalControlStatus.hold_angles[1] }
                 trigger={gimbal_status_timestamp} timestamp={gimbal_status_timestamp} />
         </Chart>
+
+        <h4>Tracker status</h4>
 
         <h6>Tracker output, proportional (P) control rate</h6>
         <Chart>
