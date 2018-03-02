8fea4106ba7a73bdb8059a5d266376304ca64983
Micah Elizabeth Scott <micah@misc.name>
2018-03-01 16:09:26 -0800

    Control stream and record status from gamepad

--- src/interface/gamepad.rs
+++ src/interface/gamepad.rs
@@ -1,6 +1,6 @@
 //! Bot control via a local gamepad
 
-use message::{Command, Message, ManualControlAxis};
+use message::{Command, Message, ManualControlAxis, CameraOutput};
 use controller::ControllerPort;
 use config::{SharedConfigFile, ControllerMode};
 use gilrs::{Event, Button, Axis, Gilrs};
@@ -10,6 +10,9 @@ use std::time::Duration;
 struct State {
     left_enable: bool,
     right_enable: bool,
+    gimbal_modifier: bool,
+    recording_modifier: bool,
+    livestream_modifier: bool,
     left_z: f32,
     right_z: f32,
     rel_x: f32,
@@ -23,6 +26,9 @@ impl State {
         State {
             left_enable: false,
             right_enable: false,
+            gimbal_modifier: false,
+            livestream_modifier: false,
+            recording_modifier: false,
             left_z: 0.0,
             right_z: 0.0,
             rel_x: 0.0,
@@ -36,27 +42,39 @@ impl State {
         *self = State::new();
     }
 
-    fn x_command(self: &State) -> Command {
+    fn x_command(&self) -> Command {
         Command::ManualControlValue(ManualControlAxis::RelativeX, self.rel_x)
     }
 
-    fn y_command(self: &State) -> Command {
+    fn y_command(&self) -> Command {
         Command::ManualControlValue(ManualControlAxis::RelativeY, self.rel_y)
     }
 
-    fn z_command(self: &State) -> Command {
+    fn z_command(&self) -> Command {
         Command::ManualControlValue(ManualControlAxis::RelativeZ, self.right_z - self.left_z)
     }
 
-    fn pitch_command(self: &State) -> Command {
+    fn pitch_command(&self) -> Command {
         Command::ManualControlValue(ManualControlAxis::CameraPitch, self.cam_y)
     }
 
-    fn yaw_command(self: &State) -> Command {
+    fn yaw_command(&self) -> Command {
         Command::ManualControlValue(ManualControlAxis::CameraYaw, self.cam_x)
     }
 
-    fn is_enabled(self: &State) -> bool {
+    fn on_off_command(&self, enabled: bool) -> Option<Command> {
+        if self.gimbal_modifier {
+            Some(Command::GimbalMotorEnable(enabled))
+        } else if self.recording_modifier {
+            Some(Command::CameraOutputEnable(CameraOutput::LocalRecording, enabled))
+        } else if self.livestream_modifier {
+            Some(Command::CameraOutputEnable(CameraOutput::LiveStream, enabled))
+        } else {
+            None
+        }
+    }
+
+    fn is_enabled(&self) -> bool {
         self.left_enable || self.right_enable
     }
 }
@@ -67,6 +85,12 @@ fn send_command(c: &ControllerPort, cmd: Command) {
 
 fn send_reset(c: &ControllerPort) {
     send_command(c, Command::ManualControlReset);
+}
+
+fn send_on_off_command(c: &ControllerPort, state: &State, enabled: bool) {
+    if let Some(cmd) = state.on_off_command(enabled) {
+        send_command(&c, cmd);
+    }
 }
 
 fn send_complete(c: &ControllerPort, state: &State) {
@@ -100,12 +124,20 @@ pub fn start(config: &SharedConfigFile, c: &ControllerPort) {
                     Event::ButtonPressed(Button::RightTrigger, _) => { state.right_enable = true; send_complete(&c, &state) },
                     Event::ButtonReleased(Button::RightTrigger, _) => { state.right_enable = false; send_complete(&c, &state) },
 
-                    Event::ButtonPressed(Button::Select, _) => send_command(&c, Command::GimbalMotorEnable(false)),
-                    Event::ButtonPressed(Button::Start, _) => send_command(&c, Command::GimbalMotorEnable(true)),
+                    Event::ButtonPressed(Button::DPadUp, _) => { state.gimbal_modifier = true; },
+                    Event::ButtonReleased(Button::DPadUp, _) => { state.gimbal_modifier = false; },
+                    Event::ButtonPressed(Button::DPadLeft, _) => { state.recording_modifier = true; },
+                    Event::ButtonReleased(Button::DPadLeft, _) => { state.recording_modifier = false; },
+                    Event::ButtonPressed(Button::DPadRight, _) => { state.livestream_modifier = true; },
+                    Event::ButtonReleased(Button::DPadRight, _) => { state.livestream_modifier = false; },
 
+                    Event::ButtonPressed(Button::Select, _) => send_on_off_command(&c, &state, false),
+                    Event::ButtonPressed(Button::Start, _) => send_on_off_command(&c, &state, true),
+
+                    Event::ButtonPressed(Button::West, _) => send_command(&c, Command::SetMode(ControllerMode::ManualFlyer)),
                     Event::ButtonPressed(Button::East, _) => send_command(&c, Command::SetMode(ControllerMode::Halted)),
-                    Event::ButtonPressed(Button::South, _) => send_command(&c, Command::SetMode(ControllerMode::ManualFlyer)),
-                    Event::ButtonPressed(Button::West, _) => { send_command(&c, Command::SetMode( match config.get_latest().mode {
+                    Event::ButtonPressed(Button::South, _) => send_command(&c, Command::SetMode(ControllerMode::Normal)),
+                    Event::ButtonPressed(Button::North, _) => { send_command(&c, Command::SetMode( match config.get_latest().mode {
                         ControllerMode::ManualWinch(n) => ControllerMode::ManualWinch((n + 1) % config.get_latest().winches.len()),
                         _ => ControllerMode::ManualWinch(0),
                     }))},
